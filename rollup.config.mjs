6808b39da72bec685eac4154e0c18c466c77890b
Januki <januki.i@intervest.lk>
2024-09-13 13:01:15 +0530

    Installed tslib and resolved build issue with Rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,10 +3,10 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
 export default {
-  input: 'src/index.ts', // entry point for your package
+  input: 'src/index.ts', 
   output: [
     {
-      file: 'dist/index.js', // CommonJS output
+      file: 'dist/index.js', 
       format: 'cjs',
       sourcemap: true,
     },
